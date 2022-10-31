4881c6f29b162191eafed9bcf7a2d05ac86f0401
dorsha <dorsha@descope.com>
2022-10-31 15:41:14 +0200

    Add file

---
+++ tests/common.py
@@ -0,0 +1,6 @@
+defaultHeaders = {
+    "Content-Type": "application/json",
+    "x-descope-sdk-name": "python",
+    "x-descope-sdk-python-version": "3.10.8",
+    "x-descope-sdk-version": "0.0.dev3",
+}
