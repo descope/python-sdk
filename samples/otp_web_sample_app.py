d6c39cd65538647985fc37c4c917482f6679c334
Omer Cohen <omer@descope.com>
2023-02-12 12:27:42 +0200

    feat: set SameSite to Strict (#134)
    Co-authored-by: Aviad Lichtenstadt <aviad@descope.com>

--- samples/otp_web_sample_app.py
+++ samples/otp_web_sample_app.py
@@ -43,7 +43,7 @@ def set_cookie_on_response(response: Response, token: dict, cookieData: dict):
         domain=cookie_domain,
         secure=False,  # True
         httponly=True,
-        samesite="None",  # "Strict", "Lax", "None"
+        samesite="Strict",  # "Strict", "Lax", "None"
     )
 
 
