651b6e140d2984e9cd2d6ba961abb9cdbf5c5c60
guyp-descope <guyp@descope.com>
2023-06-20 15:27:58 +0300

    Fix gitleaks script

--- scripts/gitleaks/gitleaks.sh
+++ scripts/gitleaks/gitleaks.sh
@@ -3,23 +3,21 @@
 CURRENT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Run detect-secrets
-#lint_find_secrets() {
-	echo "- Running secrets check"
-	INSTALLED_SECRETS_VERSION="$(gitleaks version)"
-	if [[ -z $INSTALLED_SECRETS_VERSION ]]; then
-		echo "Installing gitleaks for the first time..."
-		brew install gitleaks
-		echo "Done installing gitleaks"
-	fi
-	echo "  - Finding leaks in git log"
-	gitleaks detect -v --redact  -c ${CURRENT_DIR}/gitleaks.toml
-	if [ $? -ne 0 ]; then
-		exit 1
-	fi
-	echo "  - Finding leaks in local repo"
-	gitleaks detect --no-git -v --redact -c ${CURRENT_DIR}/gitleaks.toml
-	if [ $? -ne 0 ]; then
-		exit 1
-	fi
-	echo "- Secrets check passed sucessfully!"
-#}
+echo "- Running secrets check"
+INSTALLED_SECRETS_VERSION="$(docker inspect ghcr.io/zricethezav/gitleaks:$GITLEAKS_VERSION)"
+if [[ -z $INSTALLED_SECRETS_VERSION ]]; then
+	echo "Installing gitleaks for the first time..."
+	git pull ghcr.io/zricethezav/gitleaks:$GITLEAKS_VERSION
+	echo "Done installing gitleaks"
+fi
+echo "  - Finding leaks in git log"
+docker run --rm -v ${CURRENT_DIR}:/conf -v ${PWD}:/code ghcr.io/zricethezav/gitleaks:$GITLEAKS_VERSION detect -v --redact --source="/code" -c /conf/gitleaks.toml
+if [ $? -ne 0 ]; then
+	exit 1
+fi
+echo "  - Finding leaks in local repo"
+docker run --rm -v ${CURRENT_DIR}:/conf -v ${PWD}:/code ghcr.io/zricethezav/gitleaks:$GITLEAKS_VERSION detect --no-git -v --redact --source="/code" -c /conf/gitleaks.toml
+if [ $? -ne 0 ]; then
+	exit 1
+fi
+echo "- Secrets check passed sucessfully!"
