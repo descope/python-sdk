4c4228fcb09affdbfe607796ffabd4590517480e
Kevin Gao <kevinjamesgao@gmail.com>
2024-01-22 21:38:55 -0800

    changed saml to sso name in test file

--- samples/sso_sample_app.py
+++ samples/sso_sample_app.py
@@ -6,8 +6,8 @@ logging.basicConfig(level=logging.INFO)
 
 
 def main():
-    project_id = "P2PD8H2nZbPk5cSJfChsc00ZCi0w"
-    tenant_id = "descope.com"
+    project_id = ""
+    tenant_id = ""
 
     try:
         descope_client = DescopeClient(project_id=project_id)
@@ -15,7 +15,7 @@ def main():
         logging.info("Going to login with SSO auth method ...")
         resp = descope_client.sso.start(tenant_id, "https://www.google.com")
         logging.info(f"sso response: {resp}")
-        
+
     except AuthException:
         raise
 
